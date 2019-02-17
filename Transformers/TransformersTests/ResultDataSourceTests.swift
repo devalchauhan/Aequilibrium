5aa934545c1e2c62c88233156b1f4a1be9d0f83e
Deval Chauhan <er.devalchauhan@gmail.com>
2019-02-17 19:41:53 +0530

    - added unit test for resultDataSource

--- Transformers/TransformersTests/ResultDataSourceTests.swift
+++ Transformers/TransformersTests/ResultDataSourceTests.swift
@@ -7,27 +7,46 @@
 //
 
 import XCTest
+@testable import Transformers
 
 class ResultDataSourceTests: XCTestCase {
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testCheckTransformerName() {
+    
+        let source : Transformer = Transformer()
+        source.name = "optimus prime"; source.team = "A"; source.strength = 1;source.intelligence = 1;source.speed = 1;source.endurance = 1;source.rank = 1;source.courage = 1;source.firepower = 1;source.skill = 1;
+
+        let destination : Transformer = Transformer()
+        destination.name = "optimus prime"; destination.team = "A"; destination.strength = 1;destination.intelligence = 1;destination.speed = 1;destination.endurance = 1;destination.rank = 1;destination.courage = 1;destination.firepower = 1;destination.skill = 1;
+        
+        let name = ResultDataSource(_tableView: UITableView()).checkTransformerName(source: source, destination: destination)
+        XCTAssertTrue(!name, "one of the opponent has name optimus prime or predaking")
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCompareCourageAndStrength()  {
+        let source : Transformer = Transformer()
+        source.name = "optimus prime"; source.team = "A"; source.strength = 8;source.intelligence = 1;source.speed = 1;source.endurance = 1;source.rank = 1;source.courage = 8;source.firepower = 1;source.skill = 1;
+        
+        let destination : Transformer = Transformer()
+        destination.name = "optimus prime"; destination.team = "A"; destination.strength = 4;destination.intelligence = 1;destination.speed = 1;destination.endurance = 1;destination.rank = 1;destination.courage = 4;destination.firepower = 1;destination.skill = 1;
+        
+        let result = ResultDataSource(_tableView: UITableView()).checkCourageAndStrength(sourceTranformer: source, destinationTransformer: destination)
+        XCTAssertTrue(!result, "one of the opponent wins by strength and courage")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testCompareSkill() {
+        let source : Transformer = Transformer()
+        source.name = "optimus prime"; source.team = "A"; source.strength = 8;source.intelligence = 1;source.speed = 1;source.endurance = 1;source.rank = 1;source.courage = 8;source.firepower = 1;source.skill = 4;
+        
+        let destination : Transformer = Transformer()
+        destination.name = "optimus prime"; destination.team = "A"; destination.strength = 4;destination.intelligence = 1;destination.speed = 1;destination.endurance = 1;destination.rank = 1;destination.courage = 4;destination.firepower = 1;destination.skill = 1;
+        
+        let result = ResultDataSource(_tableView: UITableView()).checkSkill(sourceTranformer: source, destinationTransformer: destination)
+        XCTAssertTrue(!result, "one of the opponent wins by skill")
+        
     }
-
 }
